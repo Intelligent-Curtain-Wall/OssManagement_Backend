9044befc45e809d5bdb301d77e4c22d4726d2922
MinmusLin <minmuslin@outlook.com>
2024-12-02 20:17:06 +0800

    Update OssController.java

--- src/main/java/intelligentcurtainwall/ossmanagement/controller/OssController.java
+++ src/main/java/intelligentcurtainwall/ossmanagement/controller/OssController.java
@@ -1,6 +1,7 @@
 package intelligentcurtainwall.ossmanagement.controller;
 
 import intelligentcurtainwall.ossmanagement.service.OssService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
@@ -20,9 +21,14 @@ public class OssController {
         this.ossService = ossService;
     }
 
-    @GetMapping("/oss/{key}")
-    public ResponseEntity<byte[]> downloadFile(@PathVariable String key) {
+    @GetMapping("/oss/**")
+    public ResponseEntity<byte[]> downloadFile(HttpServletRequest request) {
+        String fullPath = request.getRequestURI().replace(request.getContextPath(), "");
+        String key = fullPath.substring("/oss/".length());
         byte[] fileContent = ossService.getObject(bucket, key);
+        if (fileContent == null) {
+            return ResponseEntity.notFound().build();
+        }
         String fileName = key.substring(key.lastIndexOf("/") + 1);
         HttpHeaders headers = new HttpHeaders();
         headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + fileName);
